d755aeec4da0401be1a5afe8a70a258525a19411
Matthieu Grammatico <matthieugrammatico@gmail.com>
2023-03-13 14:23:03 +0100

    maj

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+const port = 3000;
+
+app.get("/toto", function(req,res){
+    res.send("salut toto")
+    }
+);
+
+app.listen(port, function(){
+    console.log('serveur listening on port : '+port);
+    }
+);
